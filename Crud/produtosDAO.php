49cc1bee6fc4de54a670af3a70e2c900e094721e
Douglas E. K. <douglas.k2005@aluno.ifsc.edu.br>
2025-02-08 19:03:02 -0300

    Crud parte 2
    fazer mais, mais tarde

--- Crud/produtosDAO.php
+++ Crud/produtosDAO.php
@@ -13,4 +13,77 @@
         }
     }
 
+    function chekConexao($connId){
+        if($connId){
+            echo "conectado";
+        }else{
+            echo "não foi possivel conectar";
+        }
+    }
+
+    function save($valor,$quantidade,$nome){
+        $db = conecta();
+
+        $sql = "insert into produto (valor,quantidade,nome) values (?,?,?,?)";
+
+        $stmt = $db ->prepare($sql);
+        $stmt->bindValue(1,$valor);
+        $stmt->bindValue(2,$quantidade);
+        $stmt->bindValue(3,$nome);
+        $stmt->execute();
+    }
+
+    function update($valor,$quantidade,$nome,$id){
+        $db = conecta();
+
+        $sql = "update usuario set valor=?,quantidade=?,nome=? where idproduto = ?";
+
+        $stmt = $db ->prepare($sql);
+        $stmt->bindValue(1,$valor);
+        $stmt->bindValue(2,$quantidade);
+        $stmt->bindValue(3,$nome);
+        $stmt->bindValue(4,$id);
+        $stmt->execute();
+    }
+
+    function delete($id){
+        $db = conecta();
+
+        $sql = "delete from usuario where idproduto = ?";
+        $stmt = $db ->prepare($sql);
+        $stmt->bindValue(1,$id);
+        $stmt->execute();
+    }
+
+    function getProdutos(){
+        $db =conecta();
+        $sql = "select * from produto";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $busca = $stmt->fetchALL(PDO::FETCH_ASSOC);
+        return $busca;
+    }
+
+    function getProduto($nome){
+        $db =conecta();
+        $sql = "select * from produto where nome like '$nome%' ";
+        $stmt = $db->prepare($sql);
+        
+        $stmt->execute();
+        $busca = $stmt->fetchALL(PDO::FETCH_ASSOC);
+        return $busca;
+    }
+
+
+    function getProdutobyid($id){
+        $db =conecta();
+        $sql = "select * from produto where idproduto = ?";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(1,$id);
+        $stmt->execute();
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $usuario;
+    }
+
+
 ?>
